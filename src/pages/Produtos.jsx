70fd7cb7ffe16396c859b3eb34193600ef3a7f59
vilardo22 <gabrielvilardo22@gmail.com>
2023-10-16 19:27:56 -0300

    Adicionado

--- src/pages/Produtos.jsx
+++ src/pages/Produtos.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import Header from '../Components/Header'
 import { ListaProdutos } from '../Components/ListaProdutos'
+import img3 from "./../assets/images/banner2.jpg";
+import { Carousel } from "react-bootstrap";
 
 const Produtos = () => {
   return (
     <div>
       <Header/>
       <br></br>
+      <Carousel>
+      <Carousel.Item>
+        <img className="d-block w-100" src={img3} alt="First slide" />
+      </Carousel.Item>
+      </Carousel>
       <ListaProdutos></ListaProdutos>
+
     </div>
   )
 }
